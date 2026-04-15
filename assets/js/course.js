f248be3df0791e17ca3bfd09f3e2616babbbfe7d
Danilo Blum <dnl.blm@gmail.com>
2026-04-15 11:58:25 -0300

    sidebar funcionando m1a1

---
+++ assets/js/course.js
@@ -0,0 +1,326 @@
+const course = {
+	title: "Ciência Aberta.",
+	modules: [
+		{
+			type: "accordion",
+			title: "Módulo 1: Fundamentos da Ciência Aberta",
+			items: [
+				{ type: "link", icon: "welcome", title: "Introdução", path: "/modulo1/introducao.html" },
+				{ type: "link", icon: "lesson", title: "Introdução à Ciência Aberta", path: "/modulo1/aula1.html" },
+				{ type: "link", icon: "lesson", title: "Barreiras a livre circulação do conhecimento e as expectativas em relação à Ciência Aberta", path: "/modulo1/aula2.html" },
+				{ type: "link", icon: "lesson", title: "Visões críticas da Ciência Aberta", path: "/modulo1/aula3.html" },
+				{ type: "link", icon: "lesson", title: "Cenário atual da Ciência Aberta no mundo e no Brasil", path: "/modulo1/aula4.html" },
+				{ type: "link", icon: "closing", title: "Encerramento", path: "/modulo1/encerramento.html" },
+			],
+		},
+		{
+			type: "accordion",
+			title: "Módulo 2: Marcos Legais",
+			items: [
+				{ type: "link", icon: "welcome", title: "Introdução", path: "/modulo2/introducao.html" },
+				{ type: "link", icon: "lesson", title: "Principais conceitos e marcos históricos do Direito Autoral", path: "/modulo2/aula1.html" },
+				{ type: "link", icon: "lesson", title: "Direitos morais, direitos patrimoniais e transferência de Direitos Autorais", path: "/modulo2/aula2.html" },
+				{ type: "link", icon: "lesson", title: "Domínio público, Transferência de direitos e licenças Creative Commons", path: "/modulo2/aula3.html" },
+				{ type: "link", icon: "lesson", title: "Marcas, desenho industrial, indicação geográfica e patentes", path: "/modulo2/aula4.html" },
+				{ type: "link", icon: "lesson", title: "Ciência Aberta e a Propriedade Industrial: entre a divulgação e sigilo de informações", path: "/modulo2/aula5.html" },
+				{ type: "link", icon: "lesson", title: "Princípios que regem o direito de acesso à informação, restrições ao acesso às informações sigilosas, a segurança da informação e a proteção de dados pessais e a pesquisa científica", path: "/modulo2/aula6.html" },
+				{ type: "link", icon: "lesson", title: "As restrições ao acesso às informações sigilosas, a segurança da informação e a proteção de dados pessoais e a pesquisa científica", path: "/modulo2/aula7.html" },
+				{ type: "link", icon: "closing", title: "Encerramento", path: "/modulo2/apresentacao.html" },
+			],
+		},
+		{
+			type: "accordion",
+			title: "Módulo 3: Acesso Aberto",
+			items: [
+				{ type: "link", icon: "welcome", title: "Introdução", path: "/modulo3/introducao.html" },
+				{ type: "link", icon: "lesson", title: "O que é Acesso Aberto?", path: "/modulo3/aula1.html" },
+				{ type: "link", icon: "lesson", title: "Implicações na Comunicação Científica", path: "/modulo3/aula2.html" },
+				{ type: "link", icon: "lesson", title: "Revisão aberta por pares", path: "/modulo3/aula3.html" },
+				{ type: "link", icon: "lesson", title: "Preprints", path: "/modulo3/aula4.html" },
+				{ type: "link", icon: "lesson", title: "Experiências latino-americanas do Acesso Aberto", path: "/modulo3/aula5.html" },
+				{ type: "link", icon: "lesson", title: "Acesso Aberto na Fiocruz", path: "/modulo3/aula6.html" },
+				{ type: "link", icon: "closing", title: "Encerramento", path: "/modulo3/apresentacao.html" },
+			],
+		},
+		{
+			type: "accordion",
+			title: "Módulo 4: Gestão, compartilhamento, abertura de dados para pesquisa",
+			items: [
+				{ type: "link", icon: "welcome", title: "Introdução", path: "/modulo4/introducao.html" },
+				{ type: "link", icon: "lesson", title: "Introdução a dados", path: "/modulo4/aula1.html" },
+				{ type: "link", icon: "lesson", title: "Gestão de dados: conceito e principais ferramentas", path: "/modulo4/aula2.html" },
+				{ type: "link", icon: "lesson", title: "Dados para pesquisa em saúde e sua proteção jurídica", path: "/modulo4/aula3.html" },
+				{ type: "link", icon: "lesson", title: "Compartilhamento e uso dos dados para pesquisa na prática", path: "/modulo4/aula4.html" },
+				{ type: "link", icon: "lesson", title: "FAIR: Dos princípios à prática", path: "/modulo4/aula5.html" },
+				{ type: "link", icon: "lesson", title: "Planos de Gestão de Dados (PGD)", path: "/modulo4/aula6.html" },
+				{ type: "link", icon: "lesson", title: "Requisitos de financiadores e revistas científicas, plataformas e repositórios de Dados, e data papers", path: "/modulo4/aula7.html" },
+				{ type: "link", icon: "lesson", title: "Integração de dados administrativos governamentais para pesquisa em saúde Pública", path: "/modulo4/aula8.html" },
+				{ type: "link", icon: "closing", title: "Encerramento", path: "/modulo4/apresentacao.html" },
+			],
+		},
+		{
+			type: "accordion",
+			title: "Módulo 5: Educação Aberta e Recursos Educacionais Abertos",
+			items: [
+				{ type: "link", icon: "welcome", title: "Introdução", path: "/modulo5/introducao.html" },
+				{ type: "link", icon: "lesson", title: "Educação e Compromisso Social", path: "/modulo5/aula1.html" },
+				{ type: "link", icon: "lesson", title: "Educação Aberta: O que significa 'aberto' em educação?", path: "/modulo5/aula2.html" },
+				{ type: "link", icon: "lesson", title: "A Educação Aberta na Fiocruz", path: "/modulo5/aula3.html" },
+				{ type: "link", icon: "lesson", title: "Educação Aberta e Educação a Distância: modelos, desafios e perspectivas", path: "/modulo5/aula4.html" },
+				{ type: "link", icon: "lesson", title: "O que são Recursos Educacionais Abertos", path: "/modulo5/aula5.html" },
+				{ type: "link", icon: "lesson", title: "Como criar REA?", path: "/modulo5/aula6.html" },
+				{ type: "link", icon: "lesson", title: "Como encontrar, usar e compartilhar REA", path: "/modulo5/aula7.html" },
+				{ type: "link", icon: "closing", title: "Encerramento", path: "/modulo5/apresentacao.html" },
+			],
+		},
+
+		{
+			type: "accordion",
+			title: "Módulo 3: Intervenções para equipes e sistemas de saúde nas práticas de LS",
+			items: [
+				// { type: "link", icon: "welcome", title: "Boas-vindas ao módulo", path: "/modulo3/apresentacao.html" },
+				{
+					type: "accordion",
+					title: "Aula 1 | A Literacia para a Saúde no cotidiano da Atenção Primária à Saúde",
+					items: [
+						{ type: "link", icon: "lesson", title: "Introdução", path: "/modulo3/aula1/introducao.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 1: Importância da Literacia para a Saúde", path: "/modulo3/aula1/topico1.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 2: Atenção Primária à Saúde, Literacia para a Saúde e a Promoção da Saúde", path: "/modulo3/aula1/topico2.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 3: Os desafios da salutogênese", path: "/modulo3/aula1/topico3.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 4: Ações para auxiliar a população na adoção de comportamentos saudáveis e na adesão do tratamento", path: "/modulo3/aula1/topico4.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 5: Considerações finais", path: "/modulo3/aula1/topico5.html" },
+						{
+							type: "modal",
+							icon: "autor",
+							title: "Sobre o(a) autor(a)",
+							modal: {
+								id: "modal-sobre-autor-m3a1", //este id tem que ser o mesmo em modal-content/modal-sidebar.html
+								title: "Sobre o(a) autor(a)",
+								size: "lg",
+								footer: "Fechar", //texto do botão, se não tiver deixar em branco
+								lazy: true,
+							},
+						},
+					],
+				},
+				{
+					type: "accordion",
+					title: "Aula 2 | Boas práticas de LS em contextos específicos e nos ciclos da vida",
+					items: [
+						{ type: "link", icon: "lesson", title: "Introdução", path: "/modulo3/aula2/introducao.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 1: Literacia para a Saúde no cenário brasileiro", path: "/modulo3/aula2/topico1.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 2: Boas práticas em Literacia para a Saúde", path: "/modulo3/aula2/topico2.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 3: Considerações finais", path: "/modulo3/aula2/topico3.html" },
+						{
+							type: "modal",
+							icon: "autor",
+							title: "Sobre o(a) autor(a)",
+							modal: {
+								id: "modal-sobre-autor-m3a2", //este id tem que ser o mesmo em modal-content/modal-sidebar.html
+								title: "Sobre o(a) autor(a)",
+								size: "lg",
+								footer: "Fechar", //texto do botão, se não tiver deixar em branco
+								lazy: true,
+							},
+						},
+					],
+				},
+				{
+					type: "accordion",
+					title: "Aula 3 | A educação em saúde e a educação popular em saúde como estratégias para a assistência à saúde na APS",
+					items: [
+						{ type: "link", icon: "lesson", title: "Introdução", path: "/modulo3/aula3/introducao.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 1: Promoção da educação na saúde", path: "/modulo3/aula3/topico1.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 2: Educação popular como pedagogia", path: "/modulo3/aula3/topico2.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 3: Programação das ações educativas e as DCNT", path: "/modulo3/aula3/topico3.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 4: Considerações finais", path: "/modulo3/aula3/topico4.html" },
+						{ type: "link", icon: "closing", title: "Encerramento do Módulo 3", path: "/modulo3/aula3/topico5.html" },
+						{ type: "link", icon: "activity", title: "Atividades", path: "/modulo3/aula3/topico6.html" },
+						{
+							type: "modal",
+							icon: "autor",
+							title: "Sobre o(a) autor(a)",
+							modal: {
+								id: "modal-sobre-autor-m3a3", //este id tem que ser o mesmo em modal-content/modal-sidebar.html
+								title: "Sobre o(a) autor(a)",
+								size: "lg",
+								footer: "Fechar", //texto do botão, se não tiver deixar em branco
+								lazy: true,
+							},
+						},
+					],
+				},
+			],
+		},
+		{
+			type: "accordion",
+			title: "Módulo 4: Ferramentas no cotidiano do trabalho das equipes da APS",
+			items: [
+				// { type: "link", icon: "welcome", title: "Boas-vindas ao módulo", path: "/modulo4/apresentacao.html" },
+				{
+					type: "accordion",
+					title: "Aula 1 | A comunicação em saúde e estratégias para melhoria na APS",
+					items: [
+						{ type: "link", icon: "lesson", title: "Introdução", path: "/modulo4/aula1/introducao.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 1: Conceito de comunicação", path: "/modulo4/aula1/topico1.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 2: Objetivos da comunicação em saúde", path: "/modulo4/aula1/topico2.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 3: Estratégias de comunicação na APS e a Literacia para a Saúde", path: "/modulo4/aula1/topico3.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 4: Considerações finais", path: "/modulo4/aula1/topico4.html" },
+						{
+							type: "modal",
+							icon: "autor",
+							title: "Sobre o(a) autor(a)",
+							modal: {
+								id: "modal-sobre-autor-m4a1", //este id tem que ser o mesmo em modal-content/modal-sidebar.html
+								title: "Sobre o(a) autor(a)",
+								size: "lg",
+								footer: "Fechar", //texto do botão, se não tiver deixar em branco
+								lazy: true,
+							},
+						},
+					],
+				},
+				{
+					type: "accordion",
+					title: "Aula 2 | As mídias e a tecnologia de informação em saúde",
+					items: [
+						{ type: "link", icon: "lesson", title: "Introdução", path: "/modulo4/aula2/introducao.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 1: Saúde Digital (eHealth): as TICs na saúde", path: "/modulo4/aula2/topico1.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 2: Mídia, Literacia e Saúde", path: "/modulo4/aula2/topico2.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 3: Internet e Saúde", path: "/modulo4/aula2/topico3.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 4: Desinformação e 'Fake News'", path: "/modulo4/aula2/topico4.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 5: Considerações finais", path: "/modulo4/aula2/topico5.html" },
+						{
+							type: "modal",
+							icon: "autor",
+							title: "Sobre o(a) autor(a)",
+							modal: {
+								id: "modal-sobre-autor-m4a2", //este id tem que ser o mesmo em modal-content/modal-sidebar.html
+								title: "Sobre o(a) autor(a)",
+								size: "lg",
+								footer: "Fechar", //texto do botão, se não tiver deixar em branco
+								lazy: true,
+							},
+						},
+					],
+				},
+				{
+					type: "accordion",
+					title: "Aula 3 | A Literacia Digital em Saúde como competência para promover saúde na APS",
+					items: [
+						{ type: "link", icon: "lesson", title: "Introdução", path: "/modulo4/aula3/introducao.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 1: A Literacia Digital e a Literacia Digital em Saúde", path: "/modulo4/aula3/topico1.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 2: A Literacia Digital em Saúde como uma competência para ampliar os níveis de LS", path: "/modulo4/aula3/topico2.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 3: Estratégias de ação para aumentar os níveis de LS por meio da Literacia Digital em Saúde", path: "/modulo4/aula3/topico3.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 4: A Literacia Digital em Saúde e sua contribuição para a ampliação da LS na APS", path: "/modulo4/aula3/topico4.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 5: Considerações finais", path: "/modulo4/aula3/topico5.html" },
+						{ type: "link", icon: "closing", title: "Encerramento do Módulo 4", path: "/modulo4/aula3/topico6.html" },
+						{ type: "link", icon: "activity", title: "Atividades", path: "/modulo4/aula3/topico7.html" },
+						{
+							type: "modal",
+							icon: "autor",
+							title: "Sobre o(a) autor(a)",
+							modal: {
+								id: "modal-sobre-autor-m4a3", //este id tem que ser o mesmo em modal-content/modal-sidebar.html
+								title: "Sobre o(a) autor(a)",
+								size: "lg",
+								footer: "Fechar", //texto do botão, se não tiver deixar em branco
+								lazy: true,
+							},
+						},
+					],
+				},
+			],
+		},
+		{
+			type: "accordion",
+			title: "Módulo 5: Autocuidado em saúde na APS: ações para promoção da saúde e qualidadede de vida",
+			items: [
+				// { type: "link", icon: "welcome", title: "Boas-vindas ao módulo", path: "/modulo5/apresentacao.html" },
+				{
+					type: "accordion",
+					title: "Aula 1 | Autocuidado no âmbito pessoal",
+					items: [
+						{ type: "link", icon: "lesson", title: "Introdução", path: "/modulo5/aula1/introducao.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 1: Ações de autocuidado na APS", path: "/modulo5/aula1/topico1.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 2: Elementos que favorecem a adoção e/ou manutenção de hábitos saudáveis na APS", path: "/modulo5/aula1/topico2.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 3: Gestão e investimento em ações de autocuidado", path: "/modulo5/aula1/topico3.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 4: Considerações finais", path: "/modulo5/aula1/topico4.html" },
+						{
+							type: "modal",
+							icon: "autor",
+							title: "Sobre o(a) autor(a)",
+							modal: {
+								id: "modal-sobre-autor-m5a1", //este id tem que ser o mesmo em modal-content/modal-sidebar.html
+								title: "Sobre o(a) autor(a)",
+								size: "lg",
+								footer: "Fechar", //texto do botão, se não tiver deixar em branco
+								lazy: true,
+							},
+						},
+					],
+				},
+				{
+					type: "accordion",
+					title: "Aula 2 | Autocuidado no âmbito profissional",
+					items: [
+						{ type: "link", icon: "lesson", title: "Introdução", path: "/modulo5/aula2/introducao.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 1: Motivação da escolha profissional", path: "/modulo5/aula2/topico1.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 2: Motivação pelo local de trabalho", path: "/modulo5/aula2/topico2.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 3: Relacionamento interprofissional", path: "/modulo5/aula2/topico3.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 4: Comunicação em saúde e na saúde", path: "/modulo5/aula2/topico4.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 5: Considerações finais", path: "/modulo5/aula2/topico5.html" },
+						{
+							type: "modal",
+							icon: "autor",
+							title: "Sobre o(a) autor(a)",
+							modal: {
+								id: "modal-sobre-autor-m5a2", //este id tem que ser o mesmo em modal-content/modal-sidebar.html
+								title: "Sobre o(a) autor(a)",
+								size: "lg",
+								footer: "Fechar", //texto do botão, se não tiver deixar em branco
+								lazy: true,
+							},
+						},
+					],
+				},
+				{
+					type: "accordion",
+					title: "Aula 3 | Relacionamentos e qualidade de vida",
+					items: [
+						{ type: "link", icon: "lesson", title: "Introdução", path: "/modulo5/aula3/introducao.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 1: Qualidade de vida para o profissional da saúde", path: "/modulo5/aula3/topico1.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 2: Relacionamentos e qualidade de vida: família, espiritualidade, vida social, criatividade, <em>hobby</em> e lazer", path: "/modulo5/aula3/topico2.html" },
+						{ type: "link", icon: "lesson", title: "Tópico 3: Considerações finais", path: "/modulo5/aula3/topico3.html" },
+						{ type: "link", icon: "closing", title: "Encerramento do Módulo 5", path: "/modulo5/aula3/topico4.html" },
+						{ type: "link", icon: "activity", title: "Atividades", path: "/modulo5/aula3/topico5.html" },
+						{
+							type: "modal",
+							icon: "autor",
+							title: "Sobre o(a) autor(a)",
+							modal: {
+								id: "modal-sobre-autor-m5a3", //este id tem que ser o mesmo em modal-content/modal-sidebar.html
+								title: "Sobre o(a) autor(a)",
+								size: "lg",
+								footer: "Fechar", //texto do botão, se não tiver deixar em branco
+								lazy: true,
+							},
+						},
+					],
+				},
+			],
+		},
+		// {
+		// 	type: "modal",
+		// 	icon: "autor",
+		// 	title: "Título do item",
+		// 	modal: {
+		// 		id: "modal-promocao-saude",
+		// 		title: "Título do modal",
+		// 		size: "lg",
+		// 		footer: "Fechar", //texto do botão, se não tiver deixar em branco
+		// 		lazy: true,
+		// 	},
+		// },
+	],
+};
